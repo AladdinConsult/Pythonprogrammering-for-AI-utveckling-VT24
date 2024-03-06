fd01b1bace62f38203e34aa1dde8e9f1eddfd7a0
AladdinConsult <aladdinconsult@gmail.com>
2024-03-06 17:36:02 +0100

    notes

--- 6 Mars/scraping4.py
+++ 6 Mars/scraping4.py
@@ -1,3 +1,4 @@
+import pyppdf.patch_pyppeteer
 import requests_html
 from bs4 import BeautifulSoup
 import time
@@ -7,7 +8,66 @@ def create_csv(filepath):
     with open(filepath, 'w') as csv_file:
         csv_file.write('date,time,last_price,change_percent\n')
 
+def append_csv(filepath, data):
+    with open(filepath, 'a') as csv_file:
+        csv_file.write(data+'\n')
+
 oids = [334578, 334593, 334580, 334584, 334586, 334591, 334581, 334577, 334603, 334597, 334602]
 url = 'https://www.avanza.se/marknadsoversikt.html'
 
 session = requests_html.HTMLSession()
+
+# Get the branch names
+page = session.get(url)
+page.html.render()
+
+if page.ok:
+    for oid in oids:
+        html = page.html.html
+        soup = BeautifulSoup(html, 'html.parser')
+        # Find the <tr> we are intrested in
+        tr = soup.find('tr', {'data-oid': oid, 'data-delayed':'true'})
+        branch = tr.find('a', {'class':'link'}).text.strip()
+        create_csv(f'./6 Mars/data/{branch}_data.csv')
+    
+    for i in range(10):
+        page = session.get(url)
+        page.html.render()
+
+        if page.ok:
+            for oid in oids:
+                html = page.html.html
+                soup = BeautifulSoup(html, 'html.parser')
+                # Find the <tr> we are intrested in
+                tr = soup.find('tr', {'data-oid': oid, 'data-delayed':'true'})
+
+                # Get the change percent
+                change_percent = tr.find('td', {'class':'changePercent'}).text.strip()
+                change_percent = change_percent.replace(',' , '.')
+                change_percent = float(change_percent)
+
+                # Get last price
+                last_price = tr.find('td', {'class': 'lastPrice'}).text.strip()
+                last_price = last_price.replace('\xa0', '').replace(',' , '.')
+                last_price = float(last_price)
+
+                # Get last updated time
+                updated = tr.find('td', {'class': 'updated'}).text.strip()
+
+                # Get current date
+                current_date =  datetime.datetime.now().strftime('%Y-%m-%d')
+
+                # Get branch name
+                branch = tr.find('a', {'class':'link'}).text.strip()
+                append_csv(f'./6 Mars/data/{branch}_data.csv', f'{current_date},{updated}, {last_price}, {change_percent}')
+
+            print(f'Done with iteration {i+1}')
+            if i == 9:
+                break
+            time.sleep(60)
+            
+
+
+
+                
+
